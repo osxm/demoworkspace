f01af52ec6cb82cefc2867311a789ffbe9981332
Chen XM <oscar999@mail.ustc.edu.cn>
2020-04-15 00:00:47 +0800

    jsp

---
+++ spring/springboot/src/main/java/cn/osxm/springboot/controller/JspController.java
@@ -0,0 +1,29 @@
+/**
+ * @Title: JspController.java
+ * @Package cn.osxm.springboot.controller
+ * @Description: TODO
+ * @author oscarchen
+ * @date 2020年4月14日
+ * @version V1.0
+ */
+package cn.osxm.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @ClassName: JspController
+ * @Description: TODO
+ * @author oscarchen
+ */
+@Controller
+public class JspController {
+	@GetMapping("/myjsp")
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView();
+        mv.addObject("msg","Hello， JSP");
+        mv.setViewName("myjsp");
+        return mv;   
+	}
+}
